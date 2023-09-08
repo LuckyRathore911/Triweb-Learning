c70d8548ab3271c235de625c513b628ea5cda8a1
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-08 09:38:49 +0530

    break statement

---
+++ JavaScript/c8 break.js
@@ -0,0 +1,24 @@
+console.log("break stament in inner loop of Nested loop\n");
+let x = 5;
+
+for (let i = 1; i <= x; i++) {
+  for (let j = 1; j <= x; j++) {
+    console.log(i, j);
+    if (j == 3) {
+      break;
+    }
+  }
+}
+
+console.log("\nbreak stament in outer loop of Nested loop\n");
+let y = 5;
+
+for (let i = 1; i <= y; i++) {
+  for (let j = 1; j <= y; j++) {
+    console.log(i, j);
+  }
+
+  if (i == 3) {
+    break;
+  }
+}
