01a7a689c1d96f399dcd42aeb69b6ab029a4bb81
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-08 09:42:26 +0530

    continue statement

---
+++ JavaScript/c9 continue.js
@@ -0,0 +1,26 @@
+console.log("continue stament in inner loop of Nested loop\n");
+let x = 5;
+
+for (let i = 1; i <= x; i++) {
+  for (let j = 1; j <= x; j++) {
+    //inner loop
+    if (j == 3) {
+      continue;
+    }
+
+    console.log(i, j);
+  }
+}
+
+console.log("continue stament in outer loop of Nested loop\n");
+let y = 5;
+for (let i = 1; i <= y; i++) {
+  //outer loop
+
+  if (i == 3) {
+    continue;
+  }
+  for (let j = 1; j <= y; j++) {
+    console.log(i, j);
+  }
+}
