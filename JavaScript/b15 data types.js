0b5eea53f23d6633597782166de1f2074e33d568
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-08 08:21:49 +0530

    data types

---
+++ JavaScript/b15 data types.js
@@ -0,0 +1,19 @@
+let undefinedVar;
+let num = 12;
+let nullVar = null;
+let str = "Hello";
+let obj = {};
+
+console.log("undefinedVar   ", undefinedVar, " ", typeof undefinedVar);
+console.log("num            ", num, " ", typeof num);
+console.log("nullVar         ", nullVar, " ", typeof nullVar);
+console.log("str            ", str, " ", typeof str);
+console.log("obj             ", obj, " ", typeof obj);
+
+/*
+undefinedVar    undefined   undefined
+num             12   number
+nullVar          null   object
+str             Hello   string
+obj              {}   object
+*/
