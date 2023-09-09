a5f15749c7bbbb141cc419483cdfcce138de4cc8
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-10 00:36:26 +0530

    unary operators

---
+++ JavaScript/48 unary operators.js
@@ -0,0 +1,15 @@
+//Unary operators
+let h = 5;
+i = -h;
+j = "2";
+console.log(+j + 3, " ", i); //unary +j
+
+//delete unary operator
+let obj = { name: "Lucky", sex: "Female" };
+console.log(obj);
+
+delete obj.sex;
+console.log(obj);
+
+//typeof unary operator
+console.log(typeof obj);
