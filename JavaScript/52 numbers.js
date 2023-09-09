545e4315fe3db407bacce3d7037e4dee0fa8e50c
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-10 01:08:16 +0530

    numbers

---
+++ JavaScript/52 numbers.js
@@ -0,0 +1,31 @@
+let n1 = 234;
+console.log(n1); //234
+
+// Don't do like this
+let n2 = 0234;
+console.log(n2); //156
+
+let n3 = 0110;
+console.log(n3); //72
+
+let n4 = 0967;
+console.log(n4); //967
+
+console.log("With Notation");
+//better to do like below
+let xb = 0b0101;
+console.log(xb);
+let xo = 0o34;
+console.log(xo);
+let xh = 0xa1;
+console.log(xh);
+let xd = 234;
+console.log(xd);
+
+let exp = 1e4;
+console.log(exp);
+
+console.log("Number Object");
+//Number object
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
