49ea041da9fd525919e9eb68a23c875d4c457593
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-10 11:59:32 +0530

    text formatting

---
+++ JavaScript/57 text formatting.js
@@ -0,0 +1,24 @@
+//String Literals
+const str = "India";
+console.log(str);
+const str1 = "India";
+console.log(str1);
+const str3 = `India`;
+console.log(str3);
+
+//Text formatting
+const description = "India is my country";
+
+const description1 = '"India" is my country';
+const description2 = "'India' is my country";
+console.log(description1);
+console.log(description2);
+
+const description3 = "'India' is my" + ' "country"';
+console.log(description3);
+
+const description4 = "'India' is my \"country\"";
+console.log(description4);
+
+const description5 = '"India" is my country \nI love my country'; //line change using \n
+console.log(description5);
