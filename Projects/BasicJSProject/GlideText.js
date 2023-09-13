fef0f712fad8692e969bae77826d34caadc307cf
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-13 14:24:23 +0530

    Project: initialized basic Glide Text project

---
+++ Projects/BasicJSProject/GlideText.js
@@ -0,0 +1,14 @@
+let initialText = document.querySelector("h1");
+let redBox = document.getElementById("red-box");
+let greenBox = document.getElementById("green-box");
+let blueBox = document.getElementById("blue-box");
+let yellowBox = document.getElementById("yellow-box");
+
+let inputTag = document.querySelector("input");
+
+let addButton = document.getElementById("add-button");
+let deleteButton = document.getElementById("delete-button");
+let redButton = document.getElementById("red-button");
+let greenButton = document.getElementById("green-button");
+let blueButton = document.getElementById("blue-button");
+let yellowButton = document.getElementById("yellow-button");
