c0db68dfd9c3956e0e2fba7c90b5cf4d019750e0
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-13 14:34:31 +0530

    Project: added basic functionality to the GlideText project

--- Projects/BasicJSProject/GlideText.js
+++ Projects/BasicJSProject/GlideText.js
@@ -4,7 +4,7 @@ let greenBox = document.getElementById("green-box");
 let blueBox = document.getElementById("blue-box");
 let yellowBox = document.getElementById("yellow-box");
 
-let inputTag = document.querySelector("input");
+let inputTag = document.querySelector("input"); //adding .value here initialized it with empty space
 
 let addButton = document.getElementById("add-button");
 let deleteButton = document.getElementById("delete-button");
@@ -12,3 +12,27 @@ let redButton = document.getElementById("red-button");
 let greenButton = document.getElementById("green-button");
 let blueButton = document.getElementById("blue-button");
 let yellowButton = document.getElementById("yellow-button");
+
+function addition() {
+  initialText.innerHTML = inputTag.value;
+}
+function deletion() {
+  initialText.innerHTML = "";
+  redBox.innerHTML = "";
+  greenBox.innerHTML = "";
+  blueBox.innerHTML = "";
+  yellowBox.innerHTML = "";
+}
+
+function red() {
+  redBox.innerHTML = inputTag.value;
+}
+function green() {
+  greenBox.innerHTML = inputTag.value;
+}
+function blue() {
+  blueBox.innerHTML = inputTag.value;
+}
+function yellow() {
+  yellowBox.innerHTML = inputTag.value;
+}
