4502f53b62ff541f6a0feccc0b4852ae3f8bf4fa
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-09-17 19:59:19 +0530

    set object

---
+++ ES6/18 set object.js
@@ -0,0 +1,15 @@
+let set = new Set();
+console.log(set);
+
+set.add(1);
+set.add(2);
+set.add(3);
+set.add(4);
+set.add(3);
+set.add(2);
+console.log(set);
+
+/*
+Set(0) {}
+Set(4) { 1, 2, 3, 4 }
+*/
