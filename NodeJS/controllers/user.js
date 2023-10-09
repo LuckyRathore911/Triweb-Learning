5e1672406eb055a469e3e95632a58d1f71c912e8
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-09 22:49:00 +0530

    async-await, try-catch, and status codes added

--- NodeJS/controllers/user.js
+++ NodeJS/controllers/user.js
@@ -1,8 +1,13 @@
 const userModel = require("../models/user");
 
-module.exports.register = (req, res) => {
-  userModel.createUser(req.body);
-  res.send(req.body.name);
+module.exports.register = async (req, res) => {
+  let insertId = await userModel.createUser(req.body);
+  if(insertId>0){
+    res.send({status:"success",data:{id:insertId}});
+  }
+  else{
+    res.send({status:"failure",message:"User registration failed!"})
+  }
 };
 
 module.exports.retrieve = (req, res) => {
--- NodeJS/models/user.js
+++ NodeJS/models/user.js
@@ -1,14 +1,24 @@
 const database = require("../util/database");
 
-module.exports.createUser = (userData) => {
+module.exports.createUser = async (userData) => {
   // console.log(userData);
 
   //table and column names are case insensitive
   let query = "INSERT INTO userNodeJS(name,email, PASSWORD) VALUES(?,?,?)";
+  let id = 0;
+  try {
+    let result = await database.execute(query, [
+      userData.name,
+      userData.email,
+      userData.password,
+    ]);
+    id = result[0].insertId;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 
-  database.execute(query, [userData.name, userData.email, userData.password]);
+  // console.log("executed query");
 
-  console.log("executed query");
-
-  return true;
+  return id;
 };
