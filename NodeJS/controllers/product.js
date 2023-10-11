4e0a57442443a06594812b8b268f168c55908d68
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-12 00:36:50 +0530

    implemented try-catch

--- NodeJS/controllers/product.js
+++ NodeJS/controllers/product.js
@@ -1,51 +1,46 @@
 const productModel = require("../models/product");
 
 module.exports.addproduct = async (req, res) => {
-  let result = await productModel.create(req.body);
-
-  insertId = result.dataValues.id;
-
-  if (insertId > 0) {
-    res.send({ status: "success", data: { id: insertId } });
-  } else {
+  try {
+    let result = await productModel.create(req.body);
+    res.send({ status: "success", data: { id: result.dataValues.id } });
+  } catch (error) {
     res.send({ status: "failure", message: "Failed to add the product!" });
   }
 };
 
 module.exports.getproduct = async (req, res) => {
-  let productDetails = await productModel.findByPk(req.body.id);
-  if (productDetails) {
+  try {
+    let productDetails = await productModel.findByPk(req.body.id);
     res.send({ status: "success", data: productDetails });
-  } else {
+  } catch (error) {
     res.send({ status: "failure", message: "Product not found!" });
   }
 };
 
 module.exports.updateproduct = async (req, res) => {
-  let productDetails = await productModel.findByPk(req.body.id);
-  productDetails.remark = req.body.remark;
-  isUpdated = await productDetails.save();
-
-  if (isUpdated) {
+  try {
+    let productDetails = await productModel.findByPk(req.body.id);
+    productDetails.remark = req.body.remark;
+    await productDetails.save();
     res.send({
       status: "success",
       message: "Updated the remark on the product successfully!",
     });
-  } else {
+  } catch (error) {
     res.send({ status: "failure", message: "Error updating the product!" });
   }
 };
 
 module.exports.deleteproduct = async (req, res) => {
-  let productDetails = await productModel.findByPk(req.body.id);
-  isDeleted = await productDetails.destroy();
-
-  if (isDeleted) {
+  try {
+    let productDetails = await productModel.findByPk(req.body.id);
+    await productDetails.destroy();
     res.send({
       status: "success",
       message: "Deleted the product successfully!",
     });
-  } else {
+  } catch (error) {
     res.send({ status: "failure", message: "Error deleting the product!" });
   }
 };
