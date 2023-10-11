8598d0d500435def3114749952c79a2149240a65
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-12 00:22:02 +0530

    added the functionalities to get, update, and delete products

--- NodeJS/controllers/product.js
+++ NodeJS/controllers/product.js
@@ -11,3 +11,41 @@ module.exports.addproduct = async (req, res) => {
     res.send({ status: "failure", message: "Failed to add the product!" });
   }
 };
+
+module.exports.getproduct = async (req, res) => {
+  let productDetails = await productModel.findByPk(req.body.id);
+  if (productDetails) {
+    res.send({ status: "success", data: productDetails });
+  } else {
+    res.send({ status: "failure", message: "Product not found!" });
+  }
+};
+
+module.exports.updateproduct = async (req, res) => {
+  let productDetails = await productModel.findByPk(req.body.id);
+  productDetails.remark = req.body.remark;
+  isUpdated = await productDetails.save();
+
+  if (isUpdated) {
+    res.send({
+      status: "success",
+      message: "Updated the remark on the product successfully!",
+    });
+  } else {
+    res.send({ status: "failure", message: "Error updating the product!" });
+  }
+};
+
+module.exports.deleteproduct = async (req, res) => {
+  let productDetails = await productModel.findByPk(req.body.id);
+  isDeleted = await productDetails.destroy();
+
+  if (isDeleted) {
+    res.send({
+      status: "success",
+      message: "Deleted the product successfully!",
+    });
+  } else {
+    res.send({ status: "failure", message: "Error deleting the product!" });
+  }
+};
--- NodeJS/routers/product.js
+++ NodeJS/routers/product.js
@@ -4,5 +4,8 @@ const productController = require("../controllers/product");
 const router = express.Router();
 
 router.post("/add", productController.addproduct);
+router.post("/get", productController.getproduct);
+router.post("/update", productController.updateproduct);
+router.post("/delete", productController.deleteproduct);
 
 module.exports = router;
