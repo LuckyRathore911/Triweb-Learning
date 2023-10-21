a3334dff439c106b43673b067005dc0e3b98163e
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-21 12:12:27 +0530

    added datatype in front of variables

--- TypeScript/src/controllers/userController.ts
+++ TypeScript/src/controllers/userController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express'
 
-import {addUserToDB} from "../models/userModel";
+import { addUserToDB } from "../models/userModel";
 
-const register = (req:Request, res:Response) => {
-  let uname = "Lucky";
-  let password = "1234";
-  let result = addUserToDB(uname, password);
+const register = (req: Request, res: Response) => {
+
+  //:string or :number is not compulsory but lets us know what type of value is required to be sent
+  let uname: string = "Lucky";
+  let password: string = "Password1234";
+  let age: number = 22;
+
+  let result = addUserToDB(uname, password, age);
   res.send(result);
 };
 
--- TypeScript/src/models/userModel.ts
+++ TypeScript/src/models/userModel.ts
@@ -1,5 +1,6 @@
-const addUserToDB = (userName:string, password:string) => {
-  return `Successfull Registration! ${userName} ${password}`;
+const addUserToDB = (userName: string, password: string, age: number) => {
+  return `Successfull Registration! ${userName}, ${password}, ${age}`;
 };
+
 
 export { addUserToDB };
