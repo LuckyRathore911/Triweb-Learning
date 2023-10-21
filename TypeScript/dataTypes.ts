356e11bdbbb908db638252707e1b320f9e36aa15
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-21 13:24:41 +0530

    interface and type alias

--- TypeScript/dataTypes.ts
+++ TypeScript/dataTypes.ts
@@ -1,9 +1,40 @@
 const ClassStructVsInterface = () => {
-  //Object Types
-  let num: { x: number; y: number } = { x: 1, y: 4 };
-  console.log(num);
+    //Object Types
+    let num: { x: number; y: number } = { x: 1, y: 4 };
+    console.log(num);
 
-  //Optional Properties
-  let num1: { x: number; y?: number } = { x: 1 };
-  console.log(num1);
+    //Optional Properties
+    let num1: { x: number; y?: number } = { x: 1 };
+    console.log(num1);
+
+    //Interfaces: another way to name an object type
+    interface Point {
+        x: number;
+        y: number;
+    }
+
+    let p1: Point = { x: 100, y: 100 };
+    let p2: Point;
+
+    //type alias
+    type MyPoint = {
+        x: number;
+        y: number;
+    };
+
+    let p3: MyPoint = { x: 100, y: 100 };
+
+    // Difference between type alias and interface is that we can redeclare interface but not type alias
+    //   interface Point {
+    //       x: number;
+    //       y: number;
+    //       z:number
+    //     }
+
+    // type MyPoint = {  //Duplicate identifier 'MyPoint'.
+    //   x: number;
+    //   y: number;
+    // }
 };
+
+
