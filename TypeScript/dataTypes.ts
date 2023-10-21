cfc164eeb45679b79d53118fddfdc852fee8c7ef
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-21 13:23:58 +0530

    optional properties

--- TypeScript/dataTypes.ts
+++ TypeScript/dataTypes.ts
@@ -1,8 +1,9 @@
 const ClassStructVsInterface = () => {
-    //Object Types
-    let num: { x: number; y: number } = { x: 1, y: 4 };
-    console.log(num);
+  //Object Types
+  let num: { x: number; y: number } = { x: 1, y: 4 };
+  console.log(num);
 
+  //Optional Properties
+  let num1: { x: number; y?: number } = { x: 1 };
+  console.log(num1);
 };
-
-
