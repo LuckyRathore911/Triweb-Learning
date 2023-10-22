25e7d8987fd88f6c8ddd1be86d87b88e2a78dd21
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-10-22 18:45:59 +0530

    created a better format for response in case of success, error, or exception

--- Projects/QuizManager/src/controllers/userController.ts
+++ Projects/QuizManager/src/controllers/userController.ts
@@ -2,10 +2,39 @@ import { Request, Response } from 'express';
 
 import { User } from '../models/userModel'
 
-const userRegister = (req: Request, res: Response) => {
-    const user = new User(req.body)
-    user.save();
-    res.send(req.body) //to model
+interface ResponseFormat {
+    status: "success" | "error",
+    data: {},
+    message: string
+}
+
+const userRegister = async (req: Request, res: Response) => {
+
+    let response: ResponseFormat;
+    try {
+        const user = new User(req.body)
+        const result = await user.save();  //data of the registered user
+
+        if (result) {
+            response = {
+                status: "success", data: { userId: result._id }, message: "Done"
+            }
+            res.send(response)
+        }
+        else {
+            response = {
+                status: "error", data: {}, message: "Not done"
+            }
+            res.send(response)
+        }
+
+    }
+    catch (error) {
+        response = {
+            status: "error", data: {}, message: "Something Went Wrong"
+        }
+        res.status(500).send(response) //to model
+    }
 
 }
 
