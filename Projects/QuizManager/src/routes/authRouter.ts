41435bffb68d4fb3c2554566693fd5f8940f9d7c
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-11-03 22:13:40 +0530

    implemented express-validator and performed validation check for user existence

--- Projects/QuizManager/src/routes/authRouter.ts
+++ Projects/QuizManager/src/routes/authRouter.ts
@@ -1,10 +1,34 @@
 import express from "express";
+import { body } from "express-validator";
 
-import { userRegister, userlogin } from "../controllers/authController";
+import { userRegister, userlogin, doesUserExist } from "../controllers/authController";
 
 const authRouter = express.Router();
 
-authRouter.post("/", userRegister); // register user
+authRouter.post("/", [
+    body('name')
+        .trim()
+        .not()
+        .isEmpty()
+        .isLength({ min: 4 })
+        .withMessage('Please enter a valid name. [minimum 4 characters long]'),
+    body("email")
+        .trim()
+        .isEmail()
+        .custom(emailId => {
+            return doesUserExist(emailId)
+                .then(status => {
+                    if (status) { 
+                        return Promise.reject("User already exists!") 
+                    }
+                })
+                .catch(err => {
+                    return Promise.reject(err)
+                });
+        })
+        .normalizeEmail()
+
+], userRegister); // register user
 authRouter.post("/login", userlogin); // login user
 
 export { authRouter };
