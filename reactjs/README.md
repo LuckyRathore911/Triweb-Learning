952cc78c0d533b70872918a69a3d65822ffc7100
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-11-07 21:48:21 +0530

    implemented routing in the react app

--- reactjs/README.md
+++ reactjs/README.md
@@ -1,6 +1,14 @@
 ### Commands
 
+**To create the project in the current folder and start it**
+
 ```bash
 npx create-react-app ./
 npm start
+```
+
+**Dependencies**
+
+```bash
+npm install react-router-dom
 ```
--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import UnderstandingProps from "./components/UnderstandingProps";
 import UseStateHook from "./components/UseStateHook";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
 
 function App() {
   const [state, setState] = useState(true);
@@ -15,6 +19,12 @@ function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contacts" element={<Contacts />}></Route>
+      </Routes>
+
       <UnderstandingProps message="Hi, there!" />
       <UnderstandingProps message="Hello guys!" />
 
