de447a9a77224441c33394bca2be8be4f2a79b58
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-11-07 22:03:58 +0530

    used Link tag for navigation to different url endpoints

--- reactjs/src/components/NavigationBar.js
+++ reactjs/src/components/NavigationBar.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function NavigationBar() {
   return (
     <header>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contacts">Contacts</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contacts">Contacts</Link>
     </header>
   );
 }
