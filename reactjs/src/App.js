970ee50ae9f8b1ddbaf056a6187326e9c70e83ae
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-11-07 20:56:10 +0530

    implemented useState hook

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 import UnderstandingProps from "./components/UnderstandingProps";
+import UseStateHook from "./components/UseStateHook";
 
 function App() {
+  const [state, setState] = useState(true);
+
+  function close() {
+    setState(false);
+  }
+  function open() {
+    setState(true);
+  }
+
   return (
     <div>
       <UnderstandingProps message="Hi, there!" />
       <UnderstandingProps message="Hello guys!" />
+
+      {state ? (
+        <UseStateHook makeClosed={close} />
+      ) : (
+        <button onClick={open}>Open</button>
+      )}
     </div>
   );
 }
---
+++ reactjs/src/components/UseStateHook.js
@@ -0,0 +1,10 @@
+function UseStateHook(props) {
+  return (
+    <div>
+      <h1>UseState Hook</h1>
+      <button onClick={props.makeClosed}>Close</button>
+    </div>
+  );
+}
+
+export default UseStateHook;
