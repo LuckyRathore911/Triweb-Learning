7bb6adbce1fee227c5b5c8113f1a6128795c0022
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-11-09 14:23:47 +0530

    implemented useNavigate hook to redirect from one page to another

--- reactjs/src/pages/AddProduct.js
+++ reactjs/src/pages/AddProduct.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import AddProductForm from "../components/products/AddProductForm";
 
 function AddProduct() {
+  const navigate = useNavigate();
+
   function addProductHandler(newProduct) {
     fetch("http://127.0.0.1:3002/product", {
       method: "POST",
@@ -9,8 +13,8 @@ function AddProduct() {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then(() => navigate("/", { replace: true }))
+      //replace doesn't maintain the path history rather replaces the current url with what is written here.
       .catch((err) => console.log(err));
   }
   return (
