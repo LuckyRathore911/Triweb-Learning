d02d9de5350a90bf54d6d0c79a22f642c444dc12
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-12-02 14:36:36 +0530

    created a parent board which has entries based on the results of smaller boards in each cell

--- Projects/tic-tac-toe/src/App.jsx
+++ Projects/tic-tac-toe/src/App.jsx
@@ -1,44 +1,57 @@
+import { useState } from "react";
 import Board from "./components/Board";
+import Winner from "./components/Winner";
 
 export default function App() {
-  return (
-    <>
-      <div className="parent-row">
-        <div className="border-right border-bottom">
-          <Board />
-        </div>
-        <div className="border-right border-bottom">
-          <Board />
-        </div>
-        <div className="border-bottom">
-          <Board />
-        </div>
-      </div>
+  const [boards, setBoards] = useState(Array(9).fill(null));
+  console.log(boards);
 
-      <div className="parent-row">
-        <div className="border-right border-bottom">
-          <Board />
-        </div>
-        <div className="border-right border-bottom">
-          <Board />
-        </div>
-        <div className="border-bottom">
-          <Board />
-        </div>
-      </div>
+  let winner = Winner(boards);
+  const isNull = (value) => value == null;
 
-      <div className="parent-row">
-        <div className="border-right">
-          <Board />
+  if (winner) {
+    return <h1>Winner: {winner}</h1>;
+  } else if (boards.some(isNull)) {
+    return (
+      <>
+        <div className="parent-row">
+          <div className="border-right border-bottom">
+            <Board setBoards={setBoards} index={0} />
+          </div>
+          <div className="border-right border-bottom">
+            <Board setBoards={setBoards} index={1} />
+          </div>
+          <div className="border-bottom">
+            <Board setBoards={setBoards} index={2} />
+          </div>
         </div>
-        <div className="border-right">
-          <Board />
+
+        <div className="parent-row">
+          <div className="border-right border-bottom">
+            <Board setBoards={setBoards} index={3} />
+          </div>
+          <div className="border-right border-bottom">
+            <Board setBoards={setBoards} index={4} />
+          </div>
+          <div className="border-bottom">
+            <Board setBoards={setBoards} index={5} />
+          </div>
         </div>
-        <div>
-          <Board />
+
+        <div className="parent-row">
+          <div className="border-right">
+            <Board setBoards={setBoards} index={6} />
+          </div>
+          <div className="border-right">
+            <Board setBoards={setBoards} index={7} />
+          </div>
+          <div>
+            <Board setBoards={setBoards} index={8} />
+          </div>
         </div>
-      </div>
-      
-    </>
-  );
+      </>
+    );
+  } else {
+    return <h1>Draw</h1>;
+  }
 }
--- Projects/tic-tac-toe/src/components/Board.jsx
+++ Projects/tic-tac-toe/src/components/Board.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import Square from "./Square";
 import Winner from "./Winner";
 
-export default function Board() {
+export default function Board({ index, setBoards }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  useEffect(() => {
+    const winner = Winner(squares);
+    if (winner) {
+      setBoards((prevBoards) => {
+        const newBoards = [...prevBoards];
+        newBoards[index] = winner;
+        return newBoards;
+      });
+    } else if (!squares.some(isNull)) {
+      setBoards((prevBoards) => {
+        const newBoards = [...prevBoards];
+        newBoards[index] = "-";
+        return newBoards;
+      });
+    }
+  }, [squares, setBoards, index]);
 
   function handleClick(i) {
     if (Winner(squares) || squares[i]) {
@@ -21,32 +37,37 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
-  const winner = Winner(squares);
-  let isNull = (value) => value == null;
+  const isNull = (value) => value == null;
 
-  if (winner) {
-    return <div className="square status">{winner}</div>;
-  } else if (squares.some(isNull)) {
-    return (
-      <>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </>
-    );
-  } else {
-    return <div className="square status">-</div>; // in case there is no winner and no empty square is left
-  }
+  return (
+    <>
+      {Winner(squares) && (
+        <div className="square status">{Winner(squares)}</div>
+      )}
+
+      {squares.some(isNull) && (
+        <>
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
+        </>
+      )}
+
+      {!Winner(squares) && !squares.some(isNull) && (
+        <div className="square status">-</div>
+      )}
+    </>
+  );
 }
