1ca1548b1849a67f2b1a777976d469963e99ed0b
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-12-02 15:40:36 +0530

    improved functionality to declare the match result

--- Projects/tic-tac-toe/src/App.jsx
+++ Projects/tic-tac-toe/src/App.jsx
@@ -7,51 +7,48 @@ export default function App() {
   console.log(boards);
 
   let winner = Winner(boards);
-  const isNull = (value) => value == null;
 
-  if (winner) {
-    return <h1>Winner: {winner}</h1>;
-  } else if (boards.some(isNull)) {
-    return (
-      <>
-        <div className="parent-row">
-          <div className="border-right border-bottom">
-            <Board setBoards={setBoards} index={0} />
-          </div>
-          <div className="border-right border-bottom">
-            <Board setBoards={setBoards} index={1} />
-          </div>
-          <div className="border-bottom">
-            <Board setBoards={setBoards} index={2} />
-          </div>
+  return (
+    <>
+      <div className="parent-row">
+        <div className="border-right border-bottom">
+          <Board setBoards={setBoards} index={0} />
         </div>
+        <div className="border-right border-bottom">
+          <Board setBoards={setBoards} index={1} />
+        </div>
+        <div className="border-bottom">
+          <Board setBoards={setBoards} index={2} />
+        </div>
+      </div>
 
-        <div className="parent-row">
-          <div className="border-right border-bottom">
-            <Board setBoards={setBoards} index={3} />
-          </div>
-          <div className="border-right border-bottom">
-            <Board setBoards={setBoards} index={4} />
-          </div>
-          <div className="border-bottom">
-            <Board setBoards={setBoards} index={5} />
-          </div>
+      <div className="parent-row">
+        <div className="border-right border-bottom">
+          <Board setBoards={setBoards} index={3} />
         </div>
+        <div className="border-right border-bottom">
+          <Board setBoards={setBoards} index={4} />
+        </div>
+        <div className="border-bottom">
+          <Board setBoards={setBoards} index={5} />
+        </div>
+      </div>
 
-        <div className="parent-row">
-          <div className="border-right">
-            <Board setBoards={setBoards} index={6} />
-          </div>
-          <div className="border-right">
-            <Board setBoards={setBoards} index={7} />
-          </div>
-          <div>
-            <Board setBoards={setBoards} index={8} />
-          </div>
+      <div className="parent-row">
+        <div className="border-right">
+          <Board setBoards={setBoards} index={6} />
         </div>
-      </>
-    );
-  } else {
-    return <h1>Draw</h1>;
-  }
+        <div className="border-right">
+          <Board setBoards={setBoards} index={7} />
+        </div>
+        <div>
+          <Board setBoards={setBoards} index={8} />
+        </div>
+      </div>
+
+      {winner == "-" && <h1>Draw</h1>}
+
+      {(winner == "X" || winner == "O") && <h1>Winner: {winner}</h1>}
+    </>
+  );
 }
--- Projects/tic-tac-toe/src/components/Board.jsx
+++ Projects/tic-tac-toe/src/components/Board.jsx
@@ -39,35 +39,29 @@ export default function Board({ index, setBoards }) {
 
   const isNull = (value) => value == null;
 
-  return (
-    <>
-      {Winner(squares) && (
-        <div className="square status">{Winner(squares)}</div>
-      )}
-
-      {squares.some(isNull) && (
-        <>
-          <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-          </div>
-          <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-          </div>
-          <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-          </div>
-        </>
-      )}
-
-      {!Winner(squares) && !squares.some(isNull) && (
-        <div className="square status">-</div>
-      )}
-    </>
-  );
+  if (Winner(squares)) {
+    return <div className="square status">{Winner(squares)}</div>;
+  } else if (squares.some(isNull)) {
+    return (
+      <>
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </>
+    );
+  } else {
+    return <div className="square status">-</div>;
+  }
 }
