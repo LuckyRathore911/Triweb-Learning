579829e81c2597f96b7f75b723dbeb288015f59d
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2024-01-21 02:04:29 +0530

    defined routes for all pages and linked the menu items from navigation to various routes

--- Projects/portfolio/client/src/components/Navigation.jsx
+++ Projects/portfolio/client/src/components/Navigation.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-  return <h1>Navigation</h1>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/experience">WorkExperience</Link>
+      <Link to="/education">Education</Link>
+      <Link to="/extra-curricular">ExtraCurricular</Link>
+      <Link to="/contact-me">ContactMe</Link>
+    </div>
+  );
 };
 
 export default Navigation;
