dfd408e911cdaf7003b2ce81c72b5de5a0215eaf
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2024-04-18 02:35:19 +0530

    created schema for the form and stored form details in MongoDB

--- Projects/portfolio/client/src/pages/ContactMe.jsx
+++ Projects/portfolio/client/src/pages/ContactMe.jsx
@@ -2,8 +2,6 @@ import MessageForm from "../components/MessageForm";
 
 function ContactMe() {
   function userInputHandler(input) {
-    console.log(input);
-
     const url = process.env.REACT_APP_BACKEND_URL
       ? process.env.REACT_APP_BACKEND_URL
       : "http://localhost:3002/contact-me";
@@ -14,7 +12,10 @@ function ContactMe() {
       headers: {
         "Content-Type": "application/json",
       },
-    });
+    })
+      .then((response) => response.json()) //received from backend
+      .then((data) => console.log(data.status))
+      .catch((err) => console.log(err));
   }
   return (
     <>
--- Projects/portfolio/server/app.js
+++ Projects/portfolio/server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const ContactForm = require("./model/ContactForm");
 
 const app = express();
 
@@ -10,9 +13,23 @@ app.get("/", (req, res) => {
   res.send("The server is running");
 });
 
-app.post("/contact-me", (req, res) => {
-  console.log(req.body);
-  res.send({});
+app.post("/contact-me", async (req, res) => {
+  try {
+    await ContactForm.create(req.body);
+    res.send({ status: "Form submitted successfully!" }); //send to frontend  .. ContactMe.jsx
+  } catch (error) {
+    console.log(error.message);
+    res.send({ status: "error", message: error.message });
+  }
 });
 
-app.listen(process.env.port);
+mongoose
+  .connect(process.env.connectionString)
+  .then(() => {
+    app.listen(process.env.port);
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    return;
+  });
