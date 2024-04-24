a8544d7452505bb60f0159cc1616333f35406bab
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2024-04-24 23:30:19 +0530

    restructured code

--- Projects/portfolio/src/components/MessageForm.jsx
+++ Projects/portfolio/src/components/MessageForm.jsx
@@ -4,11 +4,7 @@ import emailjs from "@emailjs/browser";
 
 import Styles from "../styles/MessageForm.module.css";
 
-const MessageForm = (props) => {
-  const sender_name_ref = useRef();
-  const sender_email_id_ref = useRef();
-  const sender_message_ref = useRef();
-
+const MessageForm = () => {
   const form = useRef();
   const id = process.env.REACT_APP_EMAILJS_SERVICE_ID;
   const template = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
@@ -16,17 +12,6 @@ const MessageForm = (props) => {
 
   function submitHandler(event) {
     event.preventDefault();
-
-    const sender_name = sender_name_ref.current.value;
-    const sender_email_id = sender_email_id_ref.current.value;
-    const sender_message = sender_message_ref.current.value;
-
-    const sender = {
-      sender_name,
-      sender_email_id,
-      sender_message,
-    };
-    props.userInputHandler(sender);
 
     emailjs
       .sendForm(`${id}`, `${template}`, form.current, {
@@ -47,12 +32,7 @@ const MessageForm = (props) => {
       <form className={Styles.form} ref={form} onSubmit={submitHandler}>
         <div className="">
           <label htmlFor="sender_name">Name</label>
-          <input
-            type="text"
-            id="sender_name"
-            name="sender_name"
-            ref={sender_name_ref}
-          ></input>
+          <input type="text" id="sender_name" name="sender_name"></input>
         </div>
 
         <div className="">
@@ -61,17 +41,12 @@ const MessageForm = (props) => {
             type="email"
             id="sender_email_id"
             name="sender_email_id"
-            ref={sender_email_id_ref}
           ></input>
         </div>
 
         <div className="">
           <label htmlFor="sender_message">Message</label>
-          <textarea
-            id="sender_message"
-            name="sender_message"
-            ref={sender_message_ref}
-          ></textarea>
+          <textarea id="sender_message" name="sender_message"></textarea>
         </div>
 
         <button>Send</button>
---
+++ Projects/portfolio/src/pages/ContactMe.jsx
@@ -0,0 +1,11 @@
+import MessageForm from "../components/MessageForm";
+
+function ContactMe() {
+  return (
+    <>
+      <MessageForm />
+    </>
+  );
+}
+
+export default ContactMe;
